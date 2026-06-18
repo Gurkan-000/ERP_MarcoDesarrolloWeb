718b5437b27ba4fd45a63c8bc5317bd4bca25aa6
JOSE <u22220179@utp.edu.pe>
2026-06-18 12:26:34 -0500

    migracion de template y solucion de errores

--- src/main/resources/templates/static/js/APIs_Categoria.js
+++ src/main/resources/templates/static/js/APIs_Categoria.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8083/api';
+const BASE_URL = 'https://erp-marcodesarrolloweb.onrender.com/api';
 
 const tbodyCategoria = document.getElementById("tbodyCategoria");
 const formCategoria = document.getElementById("form-cat-categorias");
